87a212332d9adf680cddf7ed55a971a03ee395ec
Patrice Freydiere <frett27@gmail.com>
2020-11-27 21:25:30 +0100

    add expression parser for ledbox

--- led_ring.py
+++ led_ring.py
@@ -1,14 +1,37 @@
-
 import traceback
 import time
 import random
+import functools
 
-black = (0 ,0 ,0)
+black = (0, 0, 0)
 
-green = (0,100,0)
-red = (100,0,0)
-blue = (0,0,100)
-white = (255,255,255)
+green = (0, 100, 0)
+red = (100, 0, 0)
+blue = (0, 0, 100)
+white = (255, 255, 255)
+
+# colors from https://materialuicolors.co/
+uipink = (233, 30, 99)
+uired = (244, 67, 54)
+uiblue = (33, 150, 243)
+uilightblue = (3, 169, 244)
+uipurple = (156, 39, 176)
+uideeppurple = (103, 58, 183)
+uiindigo = (63, 81, 181)
+uicyan = (0, 188, 212)
+uiteal = (0, 150, 136)
+uigreen = (76, 175, 80)
+uilightgreen = (139, 195, 74)
+uilime = (205, 220, 57)
+uiyellow = (255, 235, 59)
+uiamber = (255, 193, 7)
+uiorange = (255, 152, 0)
+uideeporange = (255, 87, 34)
+uibrown = (121, 85, 72)
+uigrey = (158, 158, 158)
+uibluegrey = (96, 125, 139)
+
+
 
 class LedRing:
 
@@ -17,70 +40,69 @@ class LedRing:
         self.mqtttopic = mqtttopic
         self.all_leds = 70
         self.tworing = 25
-        self.onering = int(self.tworing /2)
+        self.onering = int(self.tworing / 2)
         self.current = self.feed(self.all_leds, black)
         self.post = self.feed(self.all_leds, black)
         self.remanence = 0.8
 
-    
     def toLed(self, color):
         """
             convert color (tuple), into led screen command string
         """
-        (r ,g ,b) = color
+        (r, g, b) = color
         s = ""
         s += chr(g) + chr(r) + chr(b)
         return s
 
     def feed(self, nb, color):
         s = ""
-        (r ,g ,b) = color
-        for i in range(0 ,nb):
+        (r, g, b) = color
+        for i in range(0, nb):
             s = s + self.toLed(color)
         return s
 
     def ring(self, h, color):
         prefix = self.feed(h * int(self.onering), black)
         r = self.feed(self.onering, color)
-        suffix_size = self.all_leds - int(len(prefix ) /3) - int(len(r ) /3)
+        suffix_size = self.all_leds - int(len(prefix) / 3) - int(len(r) / 3)
         suffix = ""
         if suffix_size > 0:
             suffix = self.feed(suffix_size, black)
         message = prefix + r + suffix
         # strip result
-        return message[0:self.all_leds * 3] 
+        return message[0:self.all_leds * 3]
 
-    # create dots patterns frame
+        # create dots patterns frame
+
     def space(self, color, length=10, shift=0, space=10):
         s = ""
         while len(s) < self.all_leds * 3:
-
             spacel = self.feed(int(space), black)
             r = self.feed(length, color)
             s = self.feed(shift, black) + r + spacel + s
 
         # strip result
-        return s[0:self.all_leds * 3] 
+        return s[0:self.all_leds * 3]
 
+        # create dots patterns
 
-    # create dots patterns
-    def dots(self, color, length = 10, shift = 0, space = 10):
+    def dots(self, color, length=10, shift=0, space=10):
         s = ""
-        while len(s) < (self.all_leds + length + space) * 2 * 3: 
+        while len(s) < (self.all_leds + length + space) * 2 * 3:
             spacel = self.feed(int(space), black)
             r = self.feed(length, color)
             s = r + spacel + s
         s = s[3 * shift:]
 
         # strip result
-        returned = s[0:self.all_leds * 3] 
+        returned = s[0:self.all_leds * 3]
         try:
             assert len(returned) == self.all_leds * 3
             return returned
         except AssertionError:
             print(len(returned))
 
-    def fade(self, led_frame, stage = 0.5):
+    def fade(self, led_frame, stage=0.5):
         """
         change the brightness level of a led frame
         :param led_frame:
@@ -102,7 +124,7 @@ class LedRing:
             led_frame2 = self.feed(self.all_leds, black)
 
         retvalue = ""
-        assert len(led_frame1) == len (led_frame2)
+        assert len(led_frame1) == len(led_frame2)
         for i in range(0, len(led_frame1)):
             v = 0
             v = ord(led_frame1[i]) + ord(led_frame2[i])
@@ -150,14 +172,15 @@ class LedRing:
     #############################################################################
     # patterns led frames
 
-    def square_pattern(self, color, nbpatterns=2, square_size=3):
+    def square_pattern(self, color, nbpatterns=2, square_size=3, shift=0):
         s2 = None
         for i in range(0, nbpatterns):
-            setpOffset = int(self.onering / nbpatterns) * i
+            setpOffset = int(self.onering / nbpatterns) * i + shift
             square = self.add(
                 self.add(self.space(color, square_size, 20 + setpOffset, 50),
-                            self.space(color, square_size, 20 + self.onering + setpOffset, 50)
-                            ), self.space(color, square_size, 20 + 2 * self.onering + setpOffset, 50))
+                         self.space(color, square_size, 20 + self.onering + setpOffset, 50)
+                         ), 
+                    self.space(color, square_size, 20 + 2 * self.onering + setpOffset, 50))
 
             if s2 is None:
                 s2 = square
@@ -165,11 +188,10 @@ class LedRing:
                 s2 = self.add(s2, square)
         return s2
 
-
     #############################################################################
     # colors generators
 
-    def linear_color(self ,fromcolor, tocolor, size = 10):
+    def linear_color(self, fromcolor, tocolor, size=10):
         """
         create a linear color generator fromcolor tocolor, with a size number of frames
         :param fromcolor:
@@ -181,17 +203,17 @@ class LedRing:
         (a2, b2, c2) = tocolor
 
         for i in range(0, size):
-            yield (int((a2 -a) / size * i + a),
+            yield (int((a2 - a) / size * i + a),
                    int((b2 - b) / size * i + b),
                    int((c2 - c) / size * i + c))
 
-    def fixed_color(self, color, framesize=20):
+    def fixed_color(self, color, framesize=5):
         """
         create a fixed color generator
         :param color:
         :return:
         """
-        for i in range(0,framesize):
+        for i in range(0, framesize):
             yield color
 
     def rainbow_color(self, size=3):
@@ -200,39 +222,49 @@ class LedRing:
         :param size:
         :return:
         """
+        isize = int(size/2)
         return self.sequence(
-            self.linear_color(blue,green, size=size),
-            self.linear_color(green, red, size=size)
+            self.linear_color(blue, green, size=isize),
+            self.linear_color(green, red, size=isize)
         )
+
+    def switch_color(self, colors =  [blue, green], times=10):
+        """ 
+        create switching colors, 
+        """
+        i = 0
+        for j in range(0,times):
+            color = colors[j % len(colors)]
+            yield color
+
 
     #############################################################################
     # frame generators
 
-    def clear(self):
+    def clear(self, nbframes = 10):
         """
         ten frame display remove
         :return: a generator with black led_frames
         """
-        empty =  self.feed(self.all_leds, black) 
-        for i in range(0 ,10):
+        empty = self.feed(self.all_leds, black)
+        for i in range(0, nbframes):
             yield empty
 
-
     # generator for moving ring on the display
-    def movering(self ,direction, color):
+    def movering(self, direction, color):
         try:
             s = ""
             assert color is not None
-            (f ,t ,s) = (0 ,6 ,1) if direction == 1 else (5 ,-1 ,-1)
+            (f, t, s) = (0, 6, 1) if direction == 1 else (5, -1, -1)
             current = self.feed(self.all_leds, black)
-            for i in range(f ,t ,s):
-                s =self.ring(i, color)
+            for i in range(f, t, s):
+                s = self.ring(i, color)
                 yield self.combine(current, s)
         except:
             traceback.print_exc()
 
     # generator for creating a color flash
-    def flash(self, color, speed = 5):
+    def flash(self, color, speed=5):
 
         buf = self.feed(self.all_leds, color)
         for i in range(0, speed):
@@ -245,25 +277,32 @@ class LedRing:
     # generator for generating a wave
     def wave(self, color):
         # wave
-        return self.sequence(self.movering(1, color), \
+        return self.sequence(self.movering(1, color),
                              self.movering(2, color))
+
     def rain(self, color):
-        return self.sequence(self.dotAnim(color,length=1,space=27), self.clear())
+        return self.sequence(self.dotAnim(color, length=1, space=27), self.clear())
 
     # generator for dots
     def dotAnim(self, color, length, space):
         for i in range(0, 2 * (length + space)):
             yield self.dots(color, length, i, space)
 
-    def colored_square(self,color_generator, nbpatterns=2, squaresize=2):
+    def colored_square(self, color_generator, nbpatterns=2, squaresize=2, shift=0):
         for i in color_generator:
-            yield  self.square_pattern(i, nbpatterns=nbpatterns, square_size=squaresize)
+            yield self.square_pattern(i, nbpatterns=nbpatterns, square_size=squaresize, shift = shift)
 
     def randomDotColor(self):
         for i in range(0, self.all_leds):
             randomColor = (g, r, b) = (random.randint(0, 80), random.randint(0, 80), random.randint(0, 80))
             yield self.add(self.pixel(i, randomColor),
-                              self.pixel(self.all_leds - i, randomColor))
+                           self.pixel(self.all_leds - i, randomColor))
+
+    def dotAnimCg(self, colorgenerator, length, space):        
+        l = map(lambda x:self.dotAnim(x,length,space), colorgenerator)
+        anim = functools.reduce(lambda x,y:self.sequence(x,y), l)
+        return anim
+
 
     ############################################################"
     # generator combiners
@@ -276,6 +315,10 @@ class LedRing:
         if not frame_generator2 is None:
             for j in frame_generator2:
                 yield j
+
+    # shift an animation
+    def shift(self, frame_generator1, shift=10):
+        return self.sequence(self.clear(shift), frame_generator1)
 
     def slow(self, frame_generator):
         for f in frame_generator:
@@ -314,6 +357,3 @@ def normaliseColor(c, level):
     v = (c1 + c2 + c3) / 3.0
     ratio = level * 1.0 / v
     return (min(int(ratio * c1), 255), (min(int(ratio * c2), 255)), (min(int(ratio * c3), 255)))
-
-
-
--- ledboxdaemon.py
+++ ledboxdaemon.py
@@ -18,10 +18,14 @@ import os.path
 import unicodedata
 
 from led_ring import *
+import led_ring
 
+import time
 
 import threading
 import queue
+
+import expression
 
 config = RawConfigParser()
 
@@ -30,11 +34,15 @@ WAVE_TOPIC="home/agents/ledbox/wave"
 DOTS_TOPIC="home/agents/ledbox/dots"
 DOTS_TOPIC2="home/agents/ledbox/dots2"
 TEST_TOPIC="home/agents/ledbox/test"
+RUN_TOPIC="home/agents/ledbox/run"
 
 WATCHDOG_TOPIC="home/agents/ledbox/watchdog"
 
 INTERRUPTOR="home/esp13/sensors/interrupt"
 PRESENCE="home/esp13/sensors/presence"
+
+MONITOR_CHECK="home/monitoring/up"
+
 
 ledring = LedRing("home/esp13/actuators/ledstrip")
 
@@ -51,19 +59,26 @@ def on_connect(client, userdata, flags, rc):
 
         # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
+
         client.subscribe(ALERTE_TOPIC)
         client.subscribe(WAVE_TOPIC)
         client.subscribe(DOTS_TOPIC)
         client.subscribe(DOTS_TOPIC2)
         client.subscribe(TEST_TOPIC)
+        client.subscribe(RUN_TOPIC)
+
         client.subscribe(INTERRUPTOR + "1")
         client.subscribe(INTERRUPTOR + "2")
         client.subscribe(INTERRUPTOR + "3")
         client.subscribe(INTERRUPTOR + "4")
         client.subscribe(PRESENCE)
+
+        client.subscribe(MONITOR_CHECK)
+
         print("End of registration")
     except:
         traceback.print_exc()
+
 
 level = 50
 red = normaliseColor((100,0,0),level)
@@ -72,7 +87,6 @@ blue = normaliseColor((0,0,100),level)
 
 pink = normaliseColor((255,192,203),level)
 purple = normaliseColor((148,0,211),level)
-
 
 def run(generator):
     assert generator != None
@@ -91,19 +105,98 @@ def decodeColor(payload):
     except:
         traceback.print_exc()
     
+monitoring_lasttime = None
+
+
+def parseExpression(ledexpression):
+
+    functions= {
+                "sequence": ledring.sequence,
+                "slow": ledring.slow,
+                "fast": ledring.fast,
+                "parallel": ledring.parallel,
+                "shift": ledring.shift,
+
+                "cglinear": ledring.linear_color,
+                "cgcolor": ledring.fixed_color,
+                "cgrainbow": ledring.rainbow_color,
+                "cgswitch": ledring.switch_color,
+
+                "clear": ledring.clear,
+                "flash": ledring.flash,
+                "rain": ledring.rain,
+                "square": ledring.colored_square,
+                "random": ledring.randomDotColor,
+                "dotanim": ledring.dotAnim,
+                "movering": ledring.movering,
+                "wave": ledring.wave
+            }
+    
+    variables = { 
+        "black":black,
+
+        "green":green,
+        "red":red,
+        "blue":blue,
+        "white":white,
+
+        # colors from https://materialuicolors.co/
+        "uipink":uipink,
+        "uired":uired,
+        "uiblue":uiblue,
+        "uilightblue":uilightblue,
+        "uipurple":uipurple,
+        "uideeppurple":uideeppurple,
+        "uiindigo":uiindigo,
+        "uicyan":uicyan,
+        "uiteal":uiteal,
+        "uigreen":uigreen,
+        "uilightgreen":uilightgreen,
+        "uilime":uilime,
+        "uiyellow":uiyellow,
+        "uiamber":uiamber,
+        "uiorange":uiorange,
+        "uideeporange":uideeporange,
+        "uibrown":uibrown,
+        "uigrey":uigrey,
+        "uibluegrey":uibluegrey}
+
+
+    parser = expression.Expression_Parser(variables=variables, functions=functions,
+                                          assignment=False)
+    e = parser.parse(ledexpression)
+    return e
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     global client2
+    global monitoring_lasttime
     try:
        # print str(msg)
        # print("message " + str(msg))
+
+       if msg.topic.startswith(MONITOR_CHECK):
+           monitoring_lasttime = time.monotonic_ns()
+
        if msg.topic.startswith(PRESENCE):
            try:
                if msg.payload == b'1':
-                   run(ledring.rain((0,10,0)))
+                   currenttime = time.monotonic_ns()
+                   if not monitoring_lasttime is None:
+                       if currenttime - monitoring_lasttime > 5_000_000_000:
+                           run(ledring.dotAnim(red,3,6))
+                           return
+                   run(ledring.rain(normaliseColor(uipurple, 10)))
            except:
                traceback.print_exc()
+       if msg.topic.startswith(RUN_TOPIC):
+           try:
+               led_expression = parseExpression(msg.payload.decode('utf-8'))
+               run(led_expression)
+           except:
+               traceback.print_exc()
+
 
        if msg.topic.startswith(INTERRUPTOR):
            try:
@@ -208,7 +301,8 @@ mqttbroker = config.get("agents","mqttbroker")
 
 client.username_pw_set(username,password)
 client.connect(mqttbroker, 1883, 5)
-client.will_set("home/agents/ledbox/lwt", payload="disconnected", qos=0, retain=False)
+client.will_set("home/agents/ledbox/lwt", 
+        payload="disconnected", qos=0, retain=False)
 
 
 class MainThread(threading.Thread):
